5738379bf87db1cf1bd2870d2136912b80df036b
DONGJIN <wew97@naver.com>
2023-08-02 15:20:39 +0900

    집..

--- swip_c_230802/10_포인터8.c
+++ swip_c_230802/10_포인터8.c
@@ -8,6 +8,7 @@
 // 2) 타입에 상관없이 동작하는 코드를 작성할 때도 사용됩니다.
 // => 포인터 연산을 수행할 수 없습니다.
 
+# if 0
 int main(void) {
 	void* p;
 
@@ -29,5 +30,39 @@ int main(void) {
 
 	p = &d;
 
+	return 0;
+}
+#endif
+
+#if 0
+int main(void) {
+	int* p;
+	printf("%p\n", p);
+
+	*p = 30; /* 미정의 동작 */
+	printf("%d\n", *p); /* 미정의 동작 */
+
+	return 0;
+}
+#endif
+
+// 1) 포인터가 유효한지 유효하지 않은지 구분하기 어렵습니다.
+// 2) 포인터가 유효하지 않은 경우,
+//    0으로 초기화해야 합니다.
+// 3) 정수 0을 사용하기보다는 NULL을 사용하는 것이 좋습니다.
+
+int main(void) {
+	//int* p = 0;
+	// 정수 0은 포인터 타입으로의 암묵적인 변환을 허용합니다.
+
+	int* p = NULL;
+
+	printf("%p\n", p);
+
+	if (p != NULL) {
+		*p = 30;
+		printf("%d\n", *p); 
+	}
+	
 	return 0;
 }
