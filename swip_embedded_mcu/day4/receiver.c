5d6d7452082ef47f32189622e3f14d2fd64b8e74
dongjin <wew97@skku.edu>
2023-08-21 07:02:54 +0100

    update can

---
+++ swip_embedded_mcu/day4/receiver.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <net/if.h>
+
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#include <linux/can.h>
+#include <linux/can/raw.h>
+
+int main(void) 
+{
+        int socketCANDescriptor; 
+        int nbytesReceived;
+        struct sockaddr_can addr;
+        struct ifreq ifr;
+        struct can_frame frame;
+        
+        char receiveMessage[8];
+               
+        printf("SocketCAN Receiver\n");
+        
+        if ((socketCANDescriptor = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
+                perror("Socket creation failed");
+                return -1;
+        }
+        
+        strcpy(ifr.ifr_name, "can0" ); 
+        ioctl(socketCANDescriptor, SIOCGIFINDEX, &ifr);
+        memset(&addr, 0, sizeof(addr));
+        
+        addr.can_family = AF_CAN;
+        addr.can_ifindex = ifr.ifr_ifindex;
+        
+        if (bind(socketCANDescriptor, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
+                perror("Bind failed");
+                return -1;
+        }
+
+        while(1) {
+                nbytesReceived = read(socketCANDescriptor, &frame, sizeof(struct can_frame));
+                if (nbytesReceived < 0) {
+                        perror("Read failed");
+                        return -1;
+                }
+
+                printf("0x%03X [%d] ", frame.can_id, frame.can_dlc);                
+                memcpy(receiveMessage, (unsigned char *)(frame.data), frame.can_dlc);
+                receiveMessage[frame.can_dlc] = '\n';
+                printf("%s", receiveMessage);
+
+                if ((receiveMessage[0] == 'q') && (frame.can_dlc == 2)) {
+                        printf("QUIT COMMAND!\n");
+                        break;
+                }
+
+                bzero(receiveMessage, 8);
+        }
+
+        if (close(socketCANDescriptor) < 0) 
+        {
+                perror("Close");
+                return -1;
+        }
+
+        return 0;
+}
