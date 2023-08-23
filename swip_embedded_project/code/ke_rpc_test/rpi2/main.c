07c54cb0a0f897278b68d62cb9f72f770ac47c15
DWKE <lkeun1119@gmail.com>
2023-08-24 08:21:46 +0900

    ke test 2

--- swip_embedded_project/code/ke_rpc_test/rpi2/main.c
+++ swip_embedded_project/code/ke_rpc_test/rpi2/main.c
@@ -18,35 +18,17 @@ int socketCANDescriptor;
 
 int main(void) 
 {
-
-    int nbytesReceived;
-    struct sockaddr_can addr;
-    struct ifreq ifr;
-    struct can_frame frame;
-    
-    char receiveMessage[8];
-            
-    printf("SocketCAN Receiver\n");
-    
-    if ((socketCANDescriptor = socket(PF_CAN, SOCK_RAW, CAN_RAW)) < 0) {
-            perror("Socket creation failed");
-            return -1;
-    }
-    
-    strcpy(ifr.ifr_name, "can0" ); 
-    ioctl(socketCANDescriptor, SIOCGIFINDEX, &ifr);
-    memset(&addr, 0, sizeof(addr));
-    
-    addr.can_family = AF_CAN;
-    addr.can_ifindex = ifr.ifr_ifindex;
-    
-    if (bind(socketCANDescriptor, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-            perror("Bind failed");
-            return -1;
+    socketCANDescriptor = setupCANSocket("can0");
+    if (socketCANDescriptor < 0)
+    {
+        return -1;
     }
 
     while(1) {
-            nbytesReceived = read(socketCANDescriptor, &frame, sizeof(struct can_frame));
+            struct can_frame frame;
+        char receiveMessage[8];
+
+            int nbytesReceived = read(socketCANDescriptor, &frame, sizeof(struct can_frame));
             if (nbytesReceived < 0) {
                     perror("Read failed");
                     return -1;
