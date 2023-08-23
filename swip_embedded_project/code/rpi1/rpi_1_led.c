31c964ca18e478bf844c17501382ebc311959162
dongjin <wew97@skku.edu>
2023-08-23 02:45:31 +0100

    update directory rpi1/rpi2

--- swip_embedded_project/code/rpi1/rpi_1_led.c
+++ swip_embedded_project/code/rpi1/rpi_1_led.c
@@ -10,17 +10,19 @@ int led_status = 0;
 
 // status = 0: default
 // status = 1: 20cm이내
-void *led(void *argumentPointer){
+void *led(void *argumentPointer)
+{
     printf("Started LED.\n\n");
     pinMode(LED_GPIO, OUTPUT);
 
-    while(1){
-        int delay_ms = led_status == 0 ? 1000 : 100;
+    int delay_ms = 1000; // default
+    while (1)
+    {
+        delay_ms = led_status == 0 ? 1000 : 100;
 
         digitalWrite(LED_GPIO, HIGH);
         delay(delay_ms);
         digitalWrite(LED_GPIO, LOW);
-        delay(delay_ms); 
+        delay(delay_ms);
     }
-    
 }
