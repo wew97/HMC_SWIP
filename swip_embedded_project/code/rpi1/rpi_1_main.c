a791ebf783b598b0b376de237bcb5196e394464a
Yoonji Kim <yoonjikim@kaist.ac.kr>
2023-08-24 05:42:15 +0100

    commit

--- swip_embedded_project/code/rpi1/rpi_1_main.c
+++ swip_embedded_project/code/rpi1/rpi_1_main.c
@@ -55,7 +55,6 @@ int main(void)
     }
 
     printf("The shortest path is %s\n", pathStr);
-    displayText(0, pathStr);
 
     // 초음파 쓰레드
     pthread_create(&threads[1], NULL, ultrasonic, NULL);
@@ -67,10 +66,10 @@ int main(void)
     {
         return -1;
     }
-
     moveMotor(120);
-    moveMotor(12);
-    // displayText(0, (const char*)buffer);
+    displayText(0, pathStr);
+    displayText(1, "You made it!\n");
+    printf("length: %d\n",pathStrLen);
     while(1)
     {
         char inputString[128];
@@ -79,12 +78,12 @@ int main(void)
 
         if ((strcmp(inputString, quit_command) == 0) && (strlen(inputString) == 5)){
             terminateRPC(quit_command);
+            printf("Terminating RPi1 #1.");
             break;
         }else{
 
-            // displayText(0, (const char*)inputString);
+            displayText(0, (const char*)inputString);
             // displayText(1, (const char*)inputString);
-            // terminateRPC("quit");
         }
     }
 
