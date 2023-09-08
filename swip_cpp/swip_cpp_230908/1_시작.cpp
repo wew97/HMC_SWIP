dea7169463d704c7695db508fd2fc5fd3dc51b9c
DONGJIN <wew97@skku.edu>
2023-09-08 09:11:50 +0900

    update cpp

---
+++ swip_cpp/swip_cpp_230908/1_시작.cpp
@@ -0,0 +1,21 @@
+// 1_시작.cpp
+
+#include <stdio.h>
+
+// 1. 확장자
+//  - C: .c
+//  - C++: .cpp, .cc, .cxx, ...
+
+// 2. 컴파일러
+//  - MSVC
+//  - GCC
+//  - LLVM
+
+// 3. C++ 표준
+//  - 1979년: C++ 탄생
+
+int main(void) {
+	printf("Hello, C\n");
+
+	return 0;
+}
